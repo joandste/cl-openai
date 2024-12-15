e377cd66c8faee4471c498e3d6a5afee6bc78792
Joandste <joandste@abo.fi>
2024-12-15 06:37:01 +0200

    it actually works

--- openai.lisp
+++ openai.lisp
@@ -9,7 +9,7 @@
     "generates response, expects an assoc list specifying arguments exactly like py/js sdk"
     (let ((json (jonathan:to-json list)))
         (dex:post openai-url :headers `(("Content-Type" . "application/json")
-                                        ("Authorization" . ,(concatenate 'string "Bearer" api-key)))
+                                        ("Authorization" . ,(concatenate 'string "Bearer " api-key)))
                              :content json)))
 
 (defun answer-user-question (question-string &key (model "gpt-4o-mini"))
@@ -17,6 +17,4 @@
     (create-completion `(:model ,model :messages (:role user :content ,question-string))))
 
 
-(create-completion '(:model "gpt-4o" :messages (:role user :content "hello gipppity")))
-
-(jonathan:to-json '(("model" . "gpt-4o-mini") ("messages" . (("role" . "user") ("content" . "hello gipppity")))) :from :alist)
+(create-completion '(:model "gpt-4o-mini" :messages (:role user :content "hello gipppity")))
